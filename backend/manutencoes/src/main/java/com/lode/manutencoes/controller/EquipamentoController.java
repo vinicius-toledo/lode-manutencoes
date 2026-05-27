bfe38ca82066b5bc35369c2c6698d257c0cb57be
Vinicius-Toledo <vini_toledo_@hotmail.com>
2026-05-27 15:16:44 -0300

    Refatoração: Implementado EquipamentoDto para retornar os dados da API com segurança

--- backend/manutencoes/src/main/java/com/lode/manutencoes/controller/EquipamentoController.java
+++ backend/manutencoes/src/main/java/com/lode/manutencoes/controller/EquipamentoController.java
@@ -1,5 +1,6 @@
 package com.lode.manutencoes.controller;
 
+import com.lode.manutencoes.Dto.EquipamentoDto;
 import com.lode.manutencoes.model.Equipamento;
 import com.lode.manutencoes.service.EquipamentoService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,46 +17,49 @@ public class EquipamentoController {
     @Autowired
     private EquipamentoService service;
 
+
     @GetMapping
-    public List<Equipamento> listarTodos(){
-        return service.listarTodos();
+    public List<EquipamentoDto> listarTodos(){
+        List<Equipamento> equipamentos = service.listarTodos();
+        return equipamentos.stream()
+                .map(EquipamentoDto::new)
+                .toList();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Equipamento> buscarPorId(@PathVariable Integer id){
+    public ResponseEntity<EquipamentoDto> buscarPorId(@PathVariable Integer id){
         return service.buscarPorId(id)
-                .map(ResponseEntity::ok)
+                .map(equipamento -> ResponseEntity.ok(new EquipamentoDto(equipamento)))
                 .orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
-    public ResponseEntity<Equipamento> salvar(@RequestBody Equipamento equipamento) {
+    public ResponseEntity<EquipamentoDto> salvar(@RequestBody Equipamento equipamento) {
         try {
             Equipamento novoEquipamento = service.salvar(equipamento);
-            return ResponseEntity.ok(novoEquipamento);
+            return ResponseEntity.ok(new EquipamentoDto(novoEquipamento));
         } catch (IllegalArgumentException e) {
             return ResponseEntity.badRequest().build();
         }
     }
 
     @PatchMapping("/{id}/status")
-    public ResponseEntity<Equipamento> alterarStatus(@PathVariable Integer id, @RequestParam String novoStatus){
+    public ResponseEntity<EquipamentoDto> alterarStatus(@PathVariable Integer id, @RequestParam String novoStatus){
         try {
             Equipamento equipamentoAtualizado = service.alterarStatus(id, novoStatus);
-            return ResponseEntity.ok(equipamentoAtualizado);
-        }catch (IllegalArgumentException e){
+            return ResponseEntity.ok(new EquipamentoDto(equipamentoAtualizado));
+        } catch (IllegalArgumentException e){
             return ResponseEntity.notFound().build();
         }
     }
 
     @DeleteMapping("/{id}")
-    public  ResponseEntity<Void> deletar(@PathVariable Integer id){
+    public ResponseEntity<Void> deletar(@PathVariable Integer id){
         try {
             service.deletar(id);
             return ResponseEntity.noContent().build();
-        }catch (IllegalArgumentException e){
+        } catch (IllegalArgumentException e){
             return ResponseEntity.notFound().build();
         }
     }
-
 }
